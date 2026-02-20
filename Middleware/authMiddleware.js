ccd49e3e4d2fe5640d30fd260f50a42aa834279c
magesh-Govinthan <mageshgovinthan16@gmail.com>
2026-02-20 12:01:13 +0530

    add

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -4,21 +4,22 @@ import User from '../Model/schema.js';
 
 dotenv.config();
 
-const authMiddleware=async (req,res,next)=>{
-  const token= req.header("Authorization")
-  if(!token){
-    return res.status(401).json({Message:"token is missing"})
-  }
-  try {
-    const decoded=jwt.verify(token,process.env.JWT_SECERT);
-    req.user=decoded;
-    const user=await User.findById(req.user._id);
-
-    if(user.role!=="admin"){
-     return res.status(401).json({Message:"Access Deny"})
+const authMiddleware = async (req, res, next) => {
+    const token = req.header('Authorization')
+    if (!token) {
+        return res.status(401).json({message:"Token is missing"})
     }
-  } catch (error) {
-    res.status(500).json({Message:"Error"})
-  }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        const user = await User.findById(req.user._id);
+
+        if (user.role != 'admin') {
+            return res.status(401).json({ message: 'Access Deny' });
+        }
+        next();
+    } catch (error) {
+        res.status(500).json({message:'Invalid Token, Internal Server Error'})
+    }
 }
 export default authMiddleware;
--- Router/userRoute.js
+++ Router/userRoute.js
@@ -6,6 +6,6 @@ const router=express.Router();
 
 router.post("/register",registerUser)
 router.post("/login",loginUser)
-router.get("/getuser",authMiddleware,getUser)
+router.get("/getUser",authMiddleware,getUser)
 
 export default router

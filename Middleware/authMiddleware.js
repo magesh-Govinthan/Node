9de7505c78c40c4a0e1fd3254852ef8ed3bb0379
magesh-Govinthan <mageshgovinthan16@gmail.com>
2026-03-09 20:24:32 +0530

    add

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -1,42 +1,41 @@
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-import User from "../Model/schema.js";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import User from '../Model/schema.js';
 
 dotenv.config();
 
 export const authMiddleware = async (req, res, next) => {
+ const authHeader = req.header("Authorization") || req.query.token;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "Token is missing" });
+  }
+
   try {
-    const authHeader = req.header("Authorization") || req.query.token;
-
-    if (!authHeader) {
-      return res.status(401).json({ success: false, message: "Token is missing" });
-    }
-
-    const token = authHeader.startsWith("Bearer ") ? authHeader.split(" ")[1] : authHeader;
+    
+     const token = authHeader.startsWith("Bearer ")
+      ? authHeader.split(" ")[1] : authHeader;
 
     if (!token) {
-      return res.status(401).json({ success: false, message: "Invalid token format" });
+      return res.status(401).json({ message: "Invalid Token Format" });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
 
-    // Flexible: _id or id
-    const userId = decoded._id || decoded.id;
-
-    const user = await User.findById(userId);
+    const user = await User.findById(req.user._id);
 
     if (!user) {
-      return res.status(404).json({ success: false, message: "User not found" });
+      return res.status(404).json({ message: "User not found" });
     }
 
     if (user.role !== "admin") {
-      return res.status(403).json({ success: false, message: "Access denied. Admin only" });
+      return res.status(403).json({ message: "Access Denied" });
     }
 
-    req.user = user; // Attach full user object
     next();
   } catch (error) {
-    return res.status(401).json({ success: false, message: "Invalid or expired token", error: error.message });
+    return res.status(401).json({ message: "Invalid or Expired Token" });
   }
 };
 
